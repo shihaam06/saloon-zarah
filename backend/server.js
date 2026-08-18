bfaeb11da178f32487190e11451fb4228d9e0804
shihaam06 <mohammedshihaam06@gmail.com>
2026-08-18 12:29:55 +0530

    ddddayyy

--- backend/server.js
+++ backend/server.js
@@ -21,12 +21,13 @@ const app = express();
 // =====================================================
 
 app.use(cors({
-    origin: [
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-        "https://saloon-zarah.vercel.app",
-        "https://kangro.in"
-    ],
+   origin: [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "https://saloon-zarah.vercel.app",
+    "https://kangro.in",
+    "https://www.kangro.in"
+],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
